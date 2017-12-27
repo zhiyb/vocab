e684a8044ac419ebdec831adbe676291f69f1540
Yubo Zhi <normanzyb@gmail.com>
2017-12-27 02:15:56 +0000

    Import Anki CSV to database

--- set/import_unit.php
+++ set/import_unit.php
@@ -7,11 +7,9 @@ if ($json == null)
     die();
 
 $id = $json['id'];
-$wid = $json['wid'];
 $unit = $json['unit'];
-$word = $json['word'];
-$info = $json['info'];
-if ($id == '' || $word == '')
+$words = $json['payload'];
+if ($id == '' || $words == '')
     die();
 if ($unit == '')
     $unit = '(default)';
@@ -35,9 +33,14 @@ if ($id == null)
 
 $ret['unit'] = $unit;
 $ret['cnt'] = 0;
-$stmt = $db->prepare('REPLACE INTO `w_' . $id . '` VALUES (?, ?, ?, ?)');
-$stmt->bind_param('isss', $wid, $unit, $word, $info);
-if ($stmt->execute() === true)
-    $ret['cnt']++;
+$stmt = $db->prepare('REPLACE INTO `w_' . $id . '` VALUES (0, ?, ?, ?)');
+foreach ($words as $e) {
+    $word = $e['word'];
+    unset($e['word']);
+    $info = json_encode($e);
+    $stmt->bind_param('sss', $unit, $word, $info);
+    if ($stmt->execute() === true)
+        $ret['cnt']++;
+}
 echo json_encode($ret);
 ?>
