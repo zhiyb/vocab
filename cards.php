6a9f3902a3d748260a3d5bb5e958cc20a0d2b6b1
Yubo Zhi <normanzyb@gmail.com>
2017-12-29 14:23:19 +0000

    Optimise stats lookup SQL queries

--- cards.php
+++ cards.php
@@ -34,7 +34,7 @@ $db->query('SET CHARACTER SET utf8');
 <li class="list-group-item list-group-item-primary">Section list</li>
 <?php
 // User stats
-$stmt = $db->prepare('SELECT `sid`, `unit`, SUM(`pass`) AS `pass`, SUM(`fail`) AS `fail` FROM ( SELECT `sid`, `unit`, (`type` = 1) AS `pass`, (`type` = -1) AS `fail` FROM ( SELECT `sid`, `unit`, LEAST( GREATEST( CAST(`yes` AS SIGNED) - CAST(`no` AS SIGNED), -1 ), 1 ) AS `type` FROM ( SELECT `user`.`id`, `sid`, `unit`, `yes`, `skip`, `no`, `time` FROM `words` RIGHT JOIN `user` ON `words`.`id` = `user`.`id` ) AS `res` HAVING (`type` <> 0) ) AS `res` ) AS `res` GROUP BY `sid`, `unit` ORDER BY `sid`, LOWER(`unit`)');
+$stmt = $db->prepare('SELECT `sid`, `unit`, SUM(`sum` > 0) AS `pass`, SUM(`sum` < 0) AS `fail` FROM ( SELECT `sid`, `unit`, ( CAST(`yes` AS SIGNED) - CAST(`no` AS SIGNED) ) AS `sum` FROM ( SELECT `user`.`id`, `sid`, `unit`, `yes`, `skip`, `no`, `time` FROM `words` RIGHT JOIN `user` ON `words`.`id` = `user`.`id` ) AS `res` ) AS `res` GROUP BY `sid`, `unit` ORDER BY `sid`, LOWER(`unit`)');
 if ($stmt->execute() !== true)
     die($stmt->error);
 $stats = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
--- get/stats_all.php
+++ get/stats_all.php
@@ -5,7 +5,7 @@ if ($db->connect_error)
     die("Connection failed: " . $db->connect_error . "\n");
 $db->query('SET CHARACTER SET utf8');
 
-$stmt = $db->prepare('SELECT `sid`, `unit`, SUM(`pass`) AS `pass`, SUM(`fail`) AS `fail` FROM ( SELECT `sid`, `unit`, (`type` = 1) AS `pass`, (`type` = -1) AS `fail` FROM ( SELECT `sid`, `unit`, LEAST( GREATEST( CAST(`yes` AS SIGNED) - CAST(`no` AS SIGNED), -1 ), 1 ) AS `type` FROM ( SELECT `user`.`id`, `sid`, `unit`, `yes`, `skip`, `no`, `time` FROM `words` RIGHT JOIN `user` ON `words`.`id` = `user`.`id` ) AS `res` HAVING (`type` <> 0) ) AS `res` ) AS `res` GROUP BY `sid`, `unit`');
+$stmt = $db->prepare('SELECT `sid`, `unit`, SUM(`sum` > 0) AS `pass`, SUM(`sum` < 0) AS `fail` FROM ( SELECT `sid`, `unit`, ( CAST(`yes` AS SIGNED) - CAST(`no` AS SIGNED) ) AS `sum` FROM ( SELECT `user`.`id`, `sid`, `unit`, `yes`, `skip`, `no`, `time` FROM `words` RIGHT JOIN `user` ON `words`.`id` = `user`.`id` ) AS `res` ) AS `res` GROUP BY `sid`, `unit` ORDER BY `sid`, LOWER(`unit`)');
 if ($stmt->execute() !== true)
     die($stmt->error);
 
